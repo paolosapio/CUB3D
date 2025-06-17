b8455c5c93b61280b5cc8b08a3d0e50c7ab3b649
Paolo Sapio <paolosapio@gmail.com>
2025-06-17 11:59:20 +0200

    compila

--- objs/srcs/gnl/get_next_line.d
+++ objs/srcs/gnl/get_next_line.d
@@ -1,2 +1,2 @@
 objs/srcs/gnl/get_next_line.o: srcs/gnl/get_next_line.c inc/cub3d.h \
-  inc/../MLX42/include/MLX42/MLX42.h libft/libft.h
+ inc/../MLX42/include/MLX42/MLX42.h libft/libft.h
--- objs/srcs/main.d
+++ objs/srcs/main.d
@@ -1,2 +1,2 @@
 objs/srcs/main.o: srcs/main.c inc/cub3d.h \
-  inc/../MLX42/include/MLX42/MLX42.h libft/libft.h
+ inc/../MLX42/include/MLX42/MLX42.h libft/libft.h
