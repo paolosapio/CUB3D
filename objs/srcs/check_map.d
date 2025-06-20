29756fc7afbd0b0fbd65d34212203837467eabdd
Anfi <ymunoz.mar@gmail.com>
2025-06-20 17:27:38 +0200

    falta el mapa

--- objs/srcs/check_map.d
+++
@@ -1,2 +0,0 @@
-objs/srcs/check_map.o: srcs/check_map.c inc/cub3d.h \
- inc/../MLX42/include/MLX42/MLX42.h libft/libft.h
--- objs/srcs/draw_game.d
+++
@@ -1,2 +0,0 @@
-objs/srcs/draw_game.o: srcs/draw_game.c inc/cub3d.h \
- inc/../MLX42/include/MLX42/MLX42.h libft/libft.h
